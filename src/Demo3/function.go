574207cf6873ea3b7c24f24c21e8a573a1060f9c
x <0>
2023-01-13 16:01:28 +0800

    1

--- src/Demo3/function.go
+++ src/Demo3/function.go
@@ -1,11 +1,15 @@
 package main
 
+import "fmt"
+
 // 函数返回值在参数列表之后
 func test(a int, b int, c string) (int, string) {
+	fmt.Println("11")
 	return a * b, c
 
 }
 
 func main() {
-
+	test(1, 2, "3")
+	fmt.Println("123")
 }
